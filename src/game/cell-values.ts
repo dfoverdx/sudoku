79ca405e6ab2744efb11ed3582add71c97d83d0c
Jordan Hitch <thinkin.arbys@gmail.com>
2019-07-07 11:35:27 -0700

    Updated cell-values to avoid unnecessary casting

--- src/game/cell-values.ts
+++ src/game/cell-values.ts
@@ -1,14 +1,14 @@
 export type CellValue = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-export const AllCellValues = new Set([ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]);
+export const AllCellValues = new Set<CellValue>([ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]);
 
 export default function* cellValues(notes?: Set<CellValue> | CellValue[]): IterableIterator<CellValue> {
     if (notes && Array.isArray(notes)) {
-        notes = new Set(notes);
+        notes = new Set<CellValue>(notes);
     }
     
-    for (let i = 1; i <= 9; i++) {
-        if (!notes || notes.has(i as CellValue)) {
-            yield i as CellValue;
+    for (let i of AllCellValues) {
+        if (!notes || notes.has(i)) {
+            yield i;
         }
     }
 }
